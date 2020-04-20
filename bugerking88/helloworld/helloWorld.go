83586462beac71f0ec735a2e85570052eac72d0c
王人葆 <wangrenbao@wangrenbaode-MacBook-Pro.local>
2020-04-20 23:37:23 +0800

    change folder

---
+++ bugerking88/helloworld/helloWorld.go
@@ -0,0 +1,11 @@
+package helloworld
+
+import (
+	"fmt"
+)
+
+func PrintHello() string {
+	fmt.Println("sy!!")
+
+	return "si"
+}
---
+++ bugerking88/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bugerking88/helloworld"
+)
+
+func main() {
+	fmt.Println("12312312312321")
+	a := 1
+	if a > 1 {
+		fmt.Println("a>1")
+	}
+	fmt.Println(helloworld.printHello())
+
+}
